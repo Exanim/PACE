b284769cd3908eb50e90991daeca38040d941f1f
Exanim <todorokiraden@gmail.com>
2023-12-25 15:28:54 +0100

    Add clay brick

---
+++ Items/Placeables/ClayBrick.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PACE.Items.Placeables;
+
+public class ClayBrick : ModItem
+{
+
+    public override void SetDefaults()
+    {
+        ((ModItem)this).Item.useStyle = 1;
+        ((ModItem)this).Item.useTurn = true;
+        ((ModItem)this).Item.useAnimation = 15;
+        ((ModItem)this).Item.useTime = 10;
+        ((ModItem)this).Item.autoReuse = true;
+        ((ModItem)this).Item.consumable = true;
+        ((Entity)((ModItem)this).Item).width = 16;
+        ((Entity)((ModItem)this).Item).height = 16;
+        ((ModItem)this).Item.maxStack = 9999;
+    }
+
+    public override void AddRecipes()
+    {
+        CreateRecipe()
+            .AddIngredient(ItemID.ClayBlock, 4)
+            .AddCondition(Condition.NearWater)
+            .Register();
+    }
+}
